277d636ef108ad2c7026be8d11b2b605bb70ffaa
prabhat8701 <87239248+NainaParashar@users.noreply.github.com>
2023-09-28 19:52:22 +0530

    first commit

---
+++ admin/adminpanel.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>NIA</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/best-carousel-slide.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/social-div-1.css">
+    <link rel="stylesheet" href="assets/css/social-div.css">
+    <link rel="stylesheet" href="assets/css/Social-Icons.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <div id="header"><img id="logo" src="assets/img/Header/National_Investigation_Agency_India_logo.png"><span class="text-white" id="initials">NIA</span><span class="text-light" id="fullname">NATIONAL INTELLIGENCE AGENCY</span></div>
+    <section id="secback">
+        <h1 class="display-3 text-white" id="tithead"><strong>Welcome To NIA</strong></h1>
+        <div id="wnia">
+            <div class="jumbotron">
+                <p>The National Investigation Agency is a central agency established by the Indian Government to combat terror in India. It acts as the Central Counter Terrorism Law Enforcement Agency. The agency is empowered to deal with terror related crimes across states without special permission from the states.</p>
+                <p><a class="btn btn-primary text-light" role="button" href="logoutadmin.php">Sign Out</a></p>
+            </div>
+        </div>
+    </section>
+    <div class="register-photo">
+        <div class="form-container">
+            <div>
+                <form class="fcid" method="post">
+                    <h2 class="text-center text-white" id="cphead"><strong>Control Panel</strong></h2>
+                    <div class="form-group"><a class="btn btn-block apb" role="button" href="petitioner.php">Petitioner Information</a></div>
+                    <div class="form-group"><a class="btn btn-block apb" role="button" href="victim.php">Victim Information</a></div>
+                    <div class="form-group"><a class="btn btn-block apb" role="button" href="fir.php">FIR Information</a></div>
+                    <div class="form-group"><a class="btn btn-block apb" role="button" href="accused.php">Accused Information</a></div>
+                    <div class="form-group"><a class="btn btn-block apb" role="button" href="case.php">Case Information</a></div>
+                    <div class="form-group"><a class="btn btn-block apb" role="button" href="caseoutcome.php">Case Outcome</a></div>
+                    <div class="form-group"><a class="btn btn-block apb" role="button" href="sectionoflaw.php">Section Of Law</a></div>
+                    <div class="form-group"><a class="btn btn-block apb" role="button" href="officer.php">Officer's Information</a></div>
+                    <div class="form-group"><a class="btn btn-block apb" role="button" href="investigate.php">Investigation Information</a></div>
+                </form>
+            </div>
+            <form class="fcid" method="post">
+            <h2 class="text-center text-white" id="rshead"><strong>Register User</strong></h2>
+                <div class="form-group"><input class="form-control" type="text" id="ruadmin" name="name" placeholder="Name"></div>
+                <div class="form-group"><input class="form-control" type="text" id="ruadmin" name="father name" placeholder="Father Name"></div>
+                <div class="form-group" style="color:white">Gender</div>
+                <div class="form-group" style="color:white"><input type="radio" name="gender" value="male" checked> Male
+                <input type="radio" name="gender" value="female"> Female
+                <input type="radio" name="gender" value="other"> Other</div>
+                <div class="form-group"><input class="form-control" type="number" id="ruadmin" name="age" placeholder="Age"></div>
+                <div class="form-group"><input class="form-control" type="tel" id="ruadmin" name="contact" placeholder="Contact No."></div>
+                <div class="form-group"><input class="form-control" type="text" id="ruadmin" name="rank" placeholder="Rank"></div>
+                <div class="form-group"><input class="form-control" type="date" id="ruadmin"  name="doa" placeholder="Date Of Appointment"></div>
+                <div class="form-group"><input class="form-control" type="text" id="ruadmin" name="street" placeholder="Street"></div>
+                <div class="form-group"><input class="form-control" type="text" id="ruadmin" name="city" placeholder="City"></div>
+                <div class="form-group"><input class="form-control" type="text" id="ruadmin" name="state" placeholder="State"></div>
+                <div class="form-group"><input class="form-control" type="email" id="ruadmin" name="email" placeholder="Email"></div>
+                <div class="form-group"><input class="form-control" type="password" id="ruadmin" name="password" placeholder="Password"></div>
+                <div class="form-group"><input class="form-control" type="password" id="ruadmin" name="password-repeat" placeholder="Retype Password"></div>
+                <div class="form-group"><button class="btn btn-block" type="submit">Sign Up</button></div>
+            </form>
+        </div>
+    </div>
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="#">Criminal Justice Information</a></li>
+                            <li><a href="#">Critical Incident Response Group</a></li>
+                            <li><a href="#">Labouratory Services</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">NIA</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>National Investigating Agency</h3>
+                        <p>Our priority&nbsp;is to help protect you, your children, your communities, <br>and your businesses from the most dangerous threats facing our <br>nation—from international and domestic terrorists to spies on Indian soil,<br> from
+                            cyber villains to corrupt government officials, from mobsters to <br>violent street gangs, from child predators to serial killers. Along the <br>way, we help defend and uphold our nation’s economy, physical and <br>electronic
+                            infrastructure, and democracy.&nbsp;<br></p>
+                    </div>
+                    <div class="col item social"><a><i class="icon ion-social-facebook"></i></a><a href="https://www.twitter.com"><i class="icon ion-social-twitter"></i></a><a href="https://www.youtube.com"><i class="icon ion-social-youtube"></i></a><a href="https://www.instagram.com"><i class="icon ion-social-instagram"></i></a></div>
+                </div>
+                <p class="copyright">NIA @ 2019</p>
+            </div>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+</body>
+
+</html>
---
+++ user/controlpanel.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>NIA</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bitter:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700">
+    <link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
+    <link rel="stylesheet" href="assets/fonts/fontawesome5-overrides.min.css">
+    <link rel="stylesheet" href="assets/css/best-carousel-slide.css">
+    <link rel="stylesheet" href="assets/css/Footer-Dark.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.5.2/animate.min.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Dark.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/social-div-1.css">
+    <link rel="stylesheet" href="assets/css/social-div.css">
+    <link rel="stylesheet" href="assets/css/Social-Icons.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <section class="wback">
+        <div id="header"><img id="logo" src="assets/img/Header/National_Investigation_Agency_India_logo.png"><span class="text-white" id="initials">NIA</span><span class="text-light" id="fullname">NATIONAL INTELLIGENCE AGENCY</span></div>
+        <div id="wnia">
+            <div class="jumbotron">
+                <h1>Welcome to NIA Control Panel</h1>
+                <p><br>The National Investigation Agency is a central agency established by the Indian Government to combat terror in India. It acts as the Central Counter Terrorism Law Enforcement Agency.&nbsp;<br><br></p>
+                <p><a class="btn btn-primary text-light" role="button" href="logout.php">Sign Out</a></p>
+            </div>
+        </div>
+        <section class="wback">
+            <div class="row">
+                <div class="col">
+                    <div class="wcen"><img class="imgs" src="assets/img/cp/respondent-petitioner-image-800x6001.jpg">
+                        <h3 class="text-white" id="hmar">Petitioner Information</h3>
+                        <p><a class="btn btn-primary text-white" role="button" href="petitioner.php" target="_parent">View</a></p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="wcen"><img class="imgs" src="assets/img/cp/190917-shugerman-victim-funds-tease_qkofhi.jpg">
+                        <h3 class="text-white" id="hmar">Victim Information</h3>
+                        <p><a class="btn btn-primary text-white" role="button" href="victim.php" target="_parent">View</a></p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="wcen"><img class="imgs" src="assets/img/cp/first-information-report.jpg">
+                        <h3 class="text-white" id="hmar">FIR Information</h3>
+                        <p><a class="btn btn-primary text-white" role="button" href="fir.php" target="_parent">View</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="wcen"><img class="imgs" src="assets/img/cp/download.jpg">
+                        <h3 class="text-white" id="hmar">Accused Information</h3>
+                        <p><a class="btn btn-primary text-white" role="button" href="accused.php" target="_parent">View</a></p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="wcen"><img class="imgs" src="assets/img/cp/14889-law.jpg">
+                        <h3 class="text-white" id="hmar">Cases Information</h3>
+                        <p><a class="btn btn-primary text-white" role="button" href="case.php" target="_parent">View</a></p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="wcen"><img class="imgs" src="assets/img/cp/cc8339f1fea7462b20f18ae9aee6c4b8.jpg">
+                        <h3 class="text-white" id="hmar">Investigating Officer</h3>
+                        <p><a class="btn btn-primary text-white" role="button" href="officer.php" target="_parent">View</a></p>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <div class="wcen"><img class="imgs" src="assets/img/cp/maxresdefault.jpg">
+                        <h3 class="text-white" id="hmar">Section Of Law</h3>
+                        <p><a class="btn btn-primary text-white" role="button" href="sectionoflaw.php" target="_parent">Admin</a></p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="wcen"><img class="imgs" src="assets/img/cp/2016-03-02%2015.44.25.png">
+                        <h3 class="text-white" id="hmar">Case Outcome</h3>
+                        <p><a class="btn btn-primary text-white" role="button" href="caseoutcome.php" target="_parent">Admin</a></p>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="wcen"><img class="imgs" src="assets/img/cp/competitor-investigation-large.jpg">
+                        <h3 class="text-white" id="hmar">Case Inquiry</h3>
+                        <p><a class="btn btn-primary text-white" role="button" href="investigate.php" target="_parent">Admin</a></p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </section>
+    <div class="footer-dark">
+        <footer>
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="#">Criminal Justice Information</a></li>
+                            <li><a href="#">Critical Incident Response Group</a></li>
+                            <li><a href="#">Labouratory Services</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-6 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">NIA</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Careers</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-md-6 item text">
+                        <h3>National Investigating Agency</h3>
+                        <p>Our priority&nbsp;is to help protect you, your children, your communities, <br>and your businesses from the most dangerous threats facing our <br>nation—from international and domestic terrorists to spies on Indian soil,<br> from
+                            cyber villains to corrupt government officials, from mobsters to <br>violent street gangs, from child predators to serial killers. Along the <br>way, we help defend and uphold our nation’s economy, physical and <br>electronic
+                            infrastructure, and democracy.&nbsp;<br></p>
+                    </div>
+                    <div class="col item social"><a><i class="icon ion-social-facebook"></i></a><a href="https://www.twitter.com"><i class="icon ion-social-twitter"></i></a><a href="https://www.youtube.com"><i class="icon ion-social-youtube"></i></a><a href="https://www.instagram.com"><i class="icon ion-social-instagram"></i></a></div>
+                </div>
+                <p class="copyright">NIA @ 2019</p>
+            </div>
+        </footer>
+    </div>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="assets/js/bs-init.js"></script>
+</body>
+
+</html>
